3142fe189d421a694ef8dad6c27e9966b0a3ffe4
Matthias Kretz <m.kretz@gsi.de>
2025-08-28 08:50:05 +0200

    Implement and test [simd.bit]
    ChangeLog:
    
    	* include/bits/simd_bit.h: Implement all functions via generator
    	constructor, calling the scalar functions.
    	* tests/simd_bit.cpp: Adjust test to use latest names. Include
    	<climits> for CHAR_BIT.

--- include/bits/simd_bit.h
+++ include/bits/simd_bit.h
@@ -17,85 +17,109 @@ namespace std::simd
     [[__gnu__::__always_inline__]]
     constexpr _Vp
     byteswap(const _Vp& __v) noexcept
-    { static_assert(false, "TODO"); }
+    { return _Vp([&](int __i) { return std::byteswap(__v[__i]); }); }
 
   template<__simd_vec_type _Vp>
     [[__gnu__::__always_inline__]]
     constexpr _Vp
     bit_ceil(const _Vp& __v) noexcept
-    { static_assert(false, "TODO"); }
+    { return _Vp([&](int __i) { return std::bit_ceil(__v[__i]); }); }
 
   template<__simd_vec_type _Vp>
     [[__gnu__::__always_inline__]]
     constexpr _Vp
     bit_floor(const _Vp& __v) noexcept
-    { static_assert(false, "TODO"); }
+    { return _Vp([&](int __i) { return std::bit_floor(__v[__i]); }); }
 
   template<__simd_vec_type _Vp>
     [[__gnu__::__always_inline__]]
     constexpr typename _Vp::mask_type
     has_single_bit(const _Vp& __v) noexcept
-    { static_assert(false, "TODO"); }
+    { return typename _Vp::mask_type([&](int __i) { return std::has_single_bit(__v[__i]); }); }
 
   template<__simd_vec_type _V0, __simd_vec_type _V1>
     [[__gnu__::__always_inline__]]
     constexpr _V0
     rotl(const _V0& __v, const _V1& __s) noexcept
-    { static_assert(false, "TODO"); }
+    { return _V0([&](int __i) { return std::rotl(__v[__i], __s[__i]); }); }
 
   template<__simd_vec_type _Vp>
     [[__gnu__::__always_inline__]]
     constexpr _Vp
     rotl(const _Vp& __v, int __s) noexcept
-    { static_assert(false, "TODO"); }
+    { return _Vp([&](int __i) { return std::rotl(__v[__i], __s); }); }
 
   template<__simd_vec_type _V0, __simd_vec_type _V1>
     [[__gnu__::__always_inline__]]
     constexpr _V0
     rotr(const _V0& __v, const _V1& __s) noexcept
-    { static_assert(false, "TODO"); }
+    { return _V0([&](int __i) { return std::rotr(__v[__i], __s[__i]); }); }
 
   template<__simd_vec_type _Vp>
     [[__gnu__::__always_inline__]]
     constexpr _Vp
     rotr(const _Vp& __v, int __s) noexcept
-    { static_assert(false, "TODO"); }
+    { return _Vp([&](int __i) { return std::rotr(__v[__i], __s); }); }
 
   template<__simd_vec_type _Vp>
     [[__gnu__::__always_inline__]]
     constexpr rebind_t<make_signed_t<typename _Vp::value_type>, _Vp>
     bit_width(const _Vp& __v) noexcept
-    { static_assert(false, "TODO"); }
+    {
+      return rebind_t<make_signed_t<typename _Vp::value_type>, _Vp>([&](int __i) {
+               return std::bit_width(__v[__i]);
+             });
+    }
 
   template<__simd_vec_type _Vp>
     [[__gnu__::__always_inline__]]
     constexpr rebind_t<make_signed_t<typename _Vp::value_type>, _Vp>
     countl_zero(const _Vp& __v) noexcept
-    { static_assert(false, "TODO"); }
+    {
+      return rebind_t<make_signed_t<typename _Vp::value_type>, _Vp>([&](int __i) {
+               return std::countl_zero(__v[__i]);
+             });
+    }
 
   template<__simd_vec_type _Vp>
     [[__gnu__::__always_inline__]]
     constexpr rebind_t<make_signed_t<typename _Vp::value_type>, _Vp>
     countl_one(const _Vp& __v) noexcept
-    { static_assert(false, "TODO"); }
+    {
+      return rebind_t<make_signed_t<typename _Vp::value_type>, _Vp>([&](int __i) {
+               return std::countl_one(__v[__i]);
+             });
+    }
 
   template<__simd_vec_type _Vp>
     [[__gnu__::__always_inline__]]
     constexpr rebind_t<make_signed_t<typename _Vp::value_type>, _Vp>
     countr_zero(const _Vp& __v) noexcept
-    { static_assert(false, "TODO"); }
+    {
+      return rebind_t<make_signed_t<typename _Vp::value_type>, _Vp>([&](int __i) {
+               return std::countr_zero(__v[__i]);
+             });
+    }
 
   template<__simd_vec_type _Vp>
     [[__gnu__::__always_inline__]]
     constexpr rebind_t<make_signed_t<typename _Vp::value_type>, _Vp>
     countr_one(const _Vp& __v) noexcept
-    { static_assert(false, "TODO"); }
+    {
+      return rebind_t<make_signed_t<typename _Vp::value_type>, _Vp>([&](int __i) {
+               return std::countr_one(__v[__i]);
+             });
+    }
 
   template<__simd_vec_type _Vp>
     [[__gnu__::__always_inline__]]
     constexpr rebind_t<make_signed_t<typename _Vp::value_type>, _Vp>
     popcount(const _Vp& __v) noexcept
-    { static_assert(false, "TODO"); }
+    {
+      return rebind_t<make_signed_t<typename _Vp::value_type>, _Vp>([&](int __i) {
+               return std::popcount(__v[__i]);
+             });
+    }
 }
 
 namespace std
