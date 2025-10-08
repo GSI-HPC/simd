2c72b08249199611f5828d3d0966241c80b41593
Matthias Kretz <m.kretz@gsi.de>
2025-10-08 08:25:09 +0200

    Remove mask reduction tests
    These tests are part of mask.cpp now.
    
    ChangeLog:
    
    	* tests/misc.cpp (mask_reductions0, mask_reductions): Remove.

--- tests/misc.cpp
+++ tests/misc.cpp
@@ -36,68 +36,6 @@ template <typename V>
       }
     };
 
-    ADD_TEST(mask_reductions0) {
-      std::tuple {test_iota<V>},
-      [](auto& t, V x) {
-        t.verify_equal(std::datapar::reduce_min_index(x == x), 0);
-        t.verify_equal(std::datapar::reduce_max_index(x == x), V::size - 1);
-      }
-    };
-
-    ADD_TEST_N(mask_reductions, int(test_iota_max<V>) + 1, requires(T x) { x + x; }) {
-      std::tuple{test_iota<V>, test_iota<V> == T(0)},
-      [](auto& t, auto ii, V v, M k0) {
-        constexpr int i = ii;
-        M k = v == T(i);
-
-        // Caveat:
-        // k0[n0 * (test_iota_max<V> + 1)] is true if it exists
-        // k[n * (test_iota_max<V> + 1) + i] is true if it exists
-        // none_of(k) is true if i > test_iota_max<V>
-        // => by test construction:
-        static_assert(i <= test_iota_max<V>);
-        // also by construction of test_iota_max:
-        static_assert(test_iota_max<V> < V::size());
-
-        constexpr int nk = 1 + (V::size() - i - 1) / (test_iota_max<V> + 1);
-        constexpr int maxk = (nk - 1) * (test_iota_max<V> + 1) + i;
-        static_assert(maxk < V::size());
-
-        constexpr int nk0 = 1 + (V::size() - 1) / (test_iota_max<V> + 1);
-        constexpr int maxk0 = (nk0 - 1) * (test_iota_max<V> + 1);
-        static_assert(maxk0 < V::size());
-
-        constexpr int maxkork0 = std::max(maxk, maxk0);
-
-        t.verify_equal(k[i], true);
-        t.verify_equal(std::as_const(k)[i], true);
-        t.verify_equal(std::datapar::reduce_min_index(k), i)(k);
-        t.verify_equal(std::datapar::reduce_max_index(k), maxk)(k);
-        t.verify_equal(std::datapar::reduce_min_index(k || k0), 0);
-        t.verify_equal(std::datapar::reduce_max_index(k || k0), maxkork0);
-        t.verify_equal(k, k);
-        t.verify_not_equal(not k, k);
-        t.verify_equal(k | k, k);
-        t.verify_equal(k & k, k);
-        t.verify(none_of(k ^ k));
-        t.verify_equal(std::datapar::reduce_count(k), nk);
-        if constexpr (sizeof(T) <= sizeof(0ULL))
-          t.verify_equal(-std::datapar::reduce(-k), nk)(k, -k);
-        t.verify_equal(std::datapar::reduce_count(not k), V::size - nk)(not k);
-        if constexpr (V::size <= 128 and sizeof(T) <= sizeof(0ULL))
-          t.verify_equal(-std::datapar::reduce(-not k), V::size - nk)(-not k);
-        t.verify(any_of(k));
-        t.verify(bool(any_of(k & k0) ^ (i != 0)));
-        k = M([&](int i) { return i == 0 ? true : k[i]; });
-        t.verify_equal(k[i], true);
-        t.verify_equal(std::as_const(k)[i], true);
-        t.verify_equal(k[0], true);
-        t.verify_equal(std::as_const(k)[0], true);
-        t.verify_equal(std::datapar::reduce_min_index(k), 0)(k);
-        t.verify_equal(std::datapar::reduce_max_index(k), maxk)(k);
-      }
-    };
-
     static_assert(dp::alignment_v<V> <= 256);
 
     ADD_TEST(loads) {
