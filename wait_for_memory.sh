2d0bc11c1f8a092b5128d2a51d4aec343b930006
Matthias Kretz <m.kretz@gsi.de>
2026-01-30 10:13:23 +0100

    Wait for at least 8 GiB of memory to be available

--- wait_for_memory.sh
+++ wait_for_memory.sh
@@ -5,6 +5,7 @@ sysfs=/sys/fs/cgroup/memory${cgroup%/*}
 #ls $sysfs
 limit=$(<$sysfs/memory.limit_in_bytes)
 usage=$sysfs/memory.usage_in_bytes
-while ((limit - $(<$usage) < 6*1024*1024*1024)); do
+while ((limit - $(<$usage) < 8*1024*1024*1024)); do
+  echo "Waiting for memory (at least 8 GiB). Avail: $(((limit - $(<$usage)) >> 20)) MiB"
   sleep 20
 done
