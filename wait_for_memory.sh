636d9d4dfaee189605ac2f7c4c99dbaa51961661
Matthias Kretz <m.kretz@gsi.de>
2026-02-03 12:31:47 +0100

    Improve wait_for_memory to wait less

--- wait_for_memory.sh
+++ wait_for_memory.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 grep -q :memory: /proc/$$/cgroup || exit 0
+[[ -z "$1" ]] && exit 0 # single job => unconditionally start
+max_jobs=$1
 cgroup=$(grep :memory: /proc/$$/cgroup | cut -d: -f3)
 sysfs=/sys/fs/cgroup/memory${cgroup%/*}
-#ls $sysfs
 limit=$(<$sysfs/memory.limit_in_bytes)
 usage=$sysfs/memory.usage_in_bytes
-while ((limit - $(<$usage) < 8*1024*1024*1024)); do
-  echo "Waiting for memory (at least 8 GiB). Avail: $(((limit - $(<$usage)) >> 20)) MiB"
+
+while true; do
+  running=$(ps h -C cc1plus|wc -l)
+  avail=$(((limit - $(<$usage)) >> 30))
+  atleast=$(((3 * (max_jobs - running)) >> 1))
+  ((avail >= atleast || 4 * running < max_jobs)) && exit 0
+  echo "Waiting for memory (at least $atleast GiB). Avail: $avail GiB"
   sleep 20
 done
